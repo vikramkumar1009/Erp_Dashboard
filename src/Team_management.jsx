b001e9980ab0b2f5101b822d7401f3dbba422a73
vikramkumar1009 <134327922+vikramkumar1009@users.noreply.github.com>
2025-03-10 15:47:48 +0530

    Update Team_management.jsx

--- src/Team_management.jsx
+++ src/Team_management.jsx
@@ -152,7 +152,7 @@ function Team_Management({ isSidebarOpen }) {
               <>
                 {/* Table Container */}
                 <div className="overflow-x-auto">
-                  <table className="w-screen border-collapse text-gray-700 text-sm md:text-lg bg-gray-50 shadow-md rounded-lg font-[Inter]">
+                  <table className="w-full border-collapse text-gray-700 text-sm md:text-lg bg-gray-50 shadow-md rounded-lg font-[Inter]">
                     <thead className="sticky top-0 bg-blue-700 text-white uppercase text-sm md:text-md leading-normal">
                       <tr>
                         <th className="py-3 px-4 text-left">ID</th>
